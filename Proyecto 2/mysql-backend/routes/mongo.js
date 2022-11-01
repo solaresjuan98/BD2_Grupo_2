707086657f1c4939ab9a06146f5e901cb03a2075
Villa01 <javillatoro1@gmail.com>
2022-11-01 02:26:50 -0600

    Agregar middleware de logs

--- Proyecto 2/mysql-backend/routes/mongo.js
+++ Proyecto 2/mysql-backend/routes/mongo.js
@@ -5,17 +5,16 @@ const viewLog = require('../middlewares/logs');
 
 const appRouter = Router();
 
-appRouter.get('/1a', mongo1_a);
- appRouter.get('/1b', mongo1_b);
- appRouter.get('/1c', mongo1_c);
-appRouter.get('/2', mongo_2);
->>>>>>> main
-appRouter.get('/3', mongo_3);
-appRouter.get('/4', mongo_4);
-appRouter.get('/5', mongo_5);
-appRouter.get('/6', mongo_6);
-appRouter.get('/7', mongo_7);
-appRouter.get('/8', mongo_8);
+appRouter.get('/1a', viewLog, mongo1_a);
+appRouter.get('/1b', viewLog, mongo1_b);
+appRouter.get('/1c', viewLog, mongo1_c);
+appRouter.get('/2', viewLog, mongo_2);
+appRouter.get('/3', viewLog, mongo_3);
+appRouter.get('/4', viewLog, mongo_4);
+appRouter.get('/5', viewLog, mongo_5);
+appRouter.get('/6', viewLog, mongo_6);
+appRouter.get('/7', viewLog, mongo_7);
+appRouter.get('/8', viewLog, mongo_8);
 
 
 module.exports = appRouter;
--- Proyecto 2/mysql-backend/routes/routes.js
+++ Proyecto 2/mysql-backend/routes/routes.js
@@ -12,20 +12,21 @@ const {
     query_7,
     query_8
 } = require('../controllers/queries.controllers');
+const viewLog = require('../middlewares/logs');
 
 const appRouter = Router();
 
 
-appRouter.get('/1a', query1_a);
-appRouter.get('/1b', query1_b);
-appRouter.get('/1c', query1_c);
-appRouter.get('/2', query_2);
-appRouter.get('/3', query_3);
-appRouter.get('/4', query_4);
-appRouter.get('/5', query_5);
-appRouter.get('/6', query_6);
-appRouter.get('/7', query_7);
-appRouter.get('/8', query_8);
+appRouter.get('/1a', viewLog, query1_a);
+appRouter.get('/1b', viewLog, query1_b);
+appRouter.get('/1c', viewLog, query1_c);
+appRouter.get('/2', viewLog, query_2);
+appRouter.get('/3', viewLog, query_3);
+appRouter.get('/4', viewLog, query_4);
+appRouter.get('/5', viewLog, query_5);
+appRouter.get('/6', viewLog, query_6);
+appRouter.get('/7', viewLog, query_7);
+appRouter.get('/8', viewLog, query_8);
 
 
 
